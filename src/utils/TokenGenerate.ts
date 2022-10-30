d3612fbe52753888e9e61e7ba4a7609142795bcf
Marcus <marcus10121@hotmail.com>
2022-10-29 23:42:50 -0300

    :boom: chore: cria classe responsável por gerar e por verificar token

---
+++ src/utils/TokenGenerate.ts
@@ -0,0 +1,18 @@
+import * as fs from 'fs/promises';
+import * as jwt from 'jsonwebtoken';
+
+export class TokenGenerate {
+  private jwtConfig: jwt.SignOptions = {
+    expiresIn: '7d',
+    algorithm: 'HS256',
+  }
+
+  public async generate(payload: object) {
+    const token = jwt.sign(payload, await fs.readFile('jwt.secret.key', 'utf-8'), this.jwtConfig)
+    return token;
+  }
+
+  public async decode(token: string) {
+    return jwt.verify(token, await fs.readFile('jwt.secret.key', 'utf-8'));
+  }
+}
