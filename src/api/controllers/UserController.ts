4b7f7b4f3227f1aa8adbdbb8566163b2ef45067e
Marcus <marcus10121@hotmail.com>
2022-11-01 14:58:33 -0300

    :goal_net: feat: Faz tratamento de erros no controller de user.

--- src/api/controllers/UserController.ts
+++ src/api/controllers/UserController.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import CustomError from "../../utils/CustomError";
+import StatusCode from "../../utils/StatusCode";
 import UserService from "../services/UserService";
 
 class UserController {
@@ -9,18 +11,43 @@ class UserController {
   }
 
   public create = async (req: Request, res: Response) => {
-    const createdUser = await this.userService.create(req.body);
-    return res.status(201).json(createdUser);
+    try {
+      const createdUser = await this.userService.create(req.body);
+      return res.status(201).json(createdUser);
+
+    } catch (error: unknown) {
+      if (error instanceof CustomError) {
+        return res.status(error.code).json({ message: error.message });
+      }
+      return res.status(StatusCode.InternalServerError).json({ message: error });
+    }
   };
 
   public login = async (req: Request, res: Response) => {
-    const login = await this.userService.login(req.body);
-    return res.status(200).json(login);
+    try {
+      const login = await this.userService.login(req.body);
+      return res.status(200).json(login);
+
+    } catch (error: unknown) {
+      if (error instanceof CustomError) {
+        return res.status(error.code).json({ message: error.message });
+      }
+      return res.status(StatusCode.InternalServerError).json({ message: error });
+    }
+
   }
 
   public getAll = async (_req: Request, res: Response) => {
-    const allUsers = await this.userService.getAll();
-    return res.status(200).json(allUsers);
+    try {
+      const allUsers = await this.userService.getAll();
+      return res.status(200).json(allUsers);
+
+    } catch (error: unknown) {
+      if (error instanceof CustomError) {
+        return res.status(error.code).json({ message: error.message });
+      }
+      return res.status(StatusCode.InternalServerError).json({ message: error });
+    }
   };
 
   public getById = async (req: Request, res: Response) => {
@@ -28,9 +55,14 @@ class UserController {
       const id = parseInt(req.params.id);
       const user = await this.userService.getById(id);
       return res.status(200).json(user);
+
     } catch (error: unknown) {
-      console.error(error);
+      if (error instanceof CustomError) {
+        return res.status(error.code).json({ message: error.message });
+      }
+      return res.status(StatusCode.InternalServerError).json({ message: error });
     }
+
   }
 
   public delete = async (req: Request, res: Response) => {
@@ -38,9 +70,14 @@ class UserController {
       const id = parseInt(req.params.id);
       await this.userService.delete(id);
       return res.status(200).json('User Deleted');
+
     } catch (error: unknown) {
-      console.error(error);
+      if (error instanceof CustomError) {
+        return res.status(error.code).json({ message: error.message });
+      }
+      return res.status(StatusCode.InternalServerError).json({ message: error });
     }
+
   }
 
   public update = async (req: Request, res: Response) => {
@@ -48,9 +85,14 @@ class UserController {
       const id = parseInt(req.params.id);
       const user = await this.userService.update(id, req.body);
       return res.status(200).json(user);
+
     } catch (error: unknown) {
-      console.error(error);
+      if (error instanceof CustomError) {
+        return res.status(error.code).json({ message: error.message });
+      }
+      return res.status(StatusCode.InternalServerError).json({ message: error });
     }
+
   }
 }
 
